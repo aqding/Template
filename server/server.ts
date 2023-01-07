5c0a6ca9577920f13e5c63c7efaf72567370e394
Allen Ding <aqding@mit.edu>
2023-01-07 15:03:09 -0600

    using http in server

--- server/server.ts
+++ server/server.ts
@@ -2,9 +2,11 @@ import express from "express";
 import { Request, Response } from "express";
 import api from "./api";
 import http from "http";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
