c363701576d68312acac3f4331a7604f6e34982d
Allen Ding <aqding@mit.edu>
2023-01-07 21:08:38 -0600

    test

--- api/index.js
+++ api/index.js
@@ -4,13 +4,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const router = express_1.default.Router();
-router.get("/test", (req, res) => {
-    console.log("Received!");
-    res.status(200).send({ message: "Server here!" });
+const api_1 = __importDefault(require("./api"));
+const http_1 = __importDefault(require("http"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+const port = process.env.PORT || 3000;
+app.use(express_1.default.json());
+app.use("/api", api_1.default);
+app.all("*", (req, res) => {
+    res.status(404).send({
+        error: "Could not find the endpoint",
+    });
 });
-router.post("/test", (req, res) => {
-    console.log(req.body);
-    res.status(200).send({ message: "Cool!" });
+const server = http_1.default.createServer(app);
+server.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
 });
-exports.default = router;
