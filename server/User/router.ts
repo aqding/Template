dbb459c4367fd1e0a841f6eeefe640f8ace33a7e
Allen Ding <aqding@mit.edu>
2023-01-10 16:37:25 -0600

    added sessions and documentation in server files

--- server/User/router.ts
+++ server/User/router.ts
@@ -1,25 +1,77 @@
 import UserModel from "./model";
 import express from "express";
-import e, { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction } from "express";
 import bcrypt from "bcrypt";
+import { useReducer } from "react";
 
 const router = express.Router();
 
+/**
+ * Get the current session.
+ *
+ * @name GET /api/user/session
+ *
+ * @return the user if they are logged in, otherwise null
+ */
+router.get("/session", async (req: Request, res: Response) => {
+  if (!req.session.userId)
+    res.status(200).json({
+      message: "You are not logged in",
+      user: null,
+    });
+  else {
+    const user = await UserModel.findById(req.session.userId);
+    res.status(200).json({
+      message: "Successfully retrieved user information from session",
+      user: user,
+    });
+  }
+});
+
+/**
+ * Create a new user. Password is stored as a salted hash
+ * in the database. This sets the session with the created
+ * user.
+ *
+ * @name POST /api/user/create
+ *
+ * @param {string} username - the username of the account
+ * @param {string} password - the password of the account
+ *
+ * @return the user that was created
+ */
 router.post("/create", async (req: Request, res: Response) => {
   const plaintextPass = req.body.password;
   const saltRounds = 5;
   bcrypt.hash(plaintextPass, saltRounds).then(async (hash) => {
     const user = new UserModel({ username: req.body.username, password: hash });
     await user.save();
+    req.session.userId = user._id.toString();
     res.status(200).json({
       message: `Successfully created user with username ${user.username}`,
+      user: user,
     });
   });
 });
 
+/**
+ * Login with a username and password. Sets the session
+ * with the user if login is successful.
+ *
+ * @name POST /api/user/login
+ *
+ * @param {string} username - the username of the account
+ * @param {string} password - the password of the account
+ *
+ * @throws {404} - if a user with the given username is not found
+ * @throws {400} - if the password is incorrect
+ *
+ * @return the user that was created
+ *
+ */
 router.post("/login", async (req: Request, res: Response) => {
   const user = await UserModel.findOne({ username: req.body.username });
-  if (!user || !user.password) {
+  if (!user) {
     res.status(404).json({
       error: "Unable to find user",
     });
@@ -27,6 +79,7 @@ router.post("/login", async (req: Request, res: Response) => {
     const hashedPass = user.password;
     bcrypt.compare(req.body.password, hashedPass).then((result) => {
       if (result) {
+        req.session.userId = user._id.toString();
         res.status(200).json({
           message: "Successfully logged in!",
         });
--- server/server.ts
+++ server/server.ts
@@ -4,38 +4,77 @@ import api from "./api";
 import http from "http";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import session from "express-session";
+import MongoStore from "connect-mongo";
 
+// Load in environment variables
 dotenv.config();
 
+// Set fields to be accessed from the req.session JSON object
+declare module "express-session" {
+  export interface SessionData {
+    userId: string;
+  }
+}
+
+// Check environment variables
 const mongoConnection = process.env.MONGO_SRV;
 if (!mongoConnection) {
   throw new Error(
     "Please add the Mongo SRV as an environment variable named MONGO_SRV"
   );
-}
+} else if (!process.env.SECRET)
+  throw new Error(
+    "Please add a session secret in the environment variable named SECRET"
+  );
 
-mongoose
+// Connect to MongoDB
+const client = mongoose
   .connect(mongoConnection)
-  .then(() => {
+  .then((mongo) => {
     console.log("Connected to Mongo");
+    return mongo.connection.getClient();
   })
   .catch((err) => {
-    `Error connecting to Mongo; ${err}`;
+    throw new Error(`Error connecting to Mongo; ${err}`);
   });
 
+// Create the express application
 const app = express();
+
+// Set the port for the server to listen to
 const port = process.env.PORT || 3000;
 
+// Allow our server to process JSON
 app.use(express.json());
 
+// Set up sessions stored in Mongo
+// See https://www.npmjs.com/package/express-session#options
+// and https://www.npmjs.com/package/connect-mongo
+// for more information
+app.use(
+  session({
+    resave: false, // Do not continiously resave session data; see line 59
+    saveUninitialized: false, // Do not save sessions that are not initialized
+    secret: process.env.SECRET, // Secret for signing cookie
+    store: MongoStore.create({
+      clientPromise: client, // Utilize previous connnection to Mongo
+      touchAfter: 24 * 3600, // Resave session data once every 24 hours
+    }),
+  })
+);
+
+// Connect all API requests
 app.use("/api", api);
 
+// Catch unmatched paths
 app.all("*", (req: Request, res: Response) => {
   res.status(404).send({
     error: "Could not find the endpoint",
   });
 });
 
+// Create the server and listen on the specified port
 const server = http.createServer(app);
 server.listen(port, () => {
   console.log(`Server listening on port ${port}`);
